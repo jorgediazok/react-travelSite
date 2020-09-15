df31ef59d586c2ac2fe60b8ec0fee7e91a7b0703
Jorge <jorgediazok@gmail.com>
2020-09-15 16:37:11 -0300

    working in hero section

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../HeroSection';
+
+import '../../App.css';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <HeroSection />
+    </React.Fragment>
+  );
+}
+
+export default Home;
