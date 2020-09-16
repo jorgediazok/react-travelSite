3b94ca628d94d9644340fbef19e771e69c9e2bac
Jorge <jorgediazok@gmail.com>
2020-09-16 00:50:37 -0300

    created 3 more pages

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -24,21 +24,21 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              src="images/img-3.jpg"
+              text="Practice Yachting in the middle of the Artic Ocean. Surreal."
+              label="Extreme"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-8.jpg"
+              text="Camp in the Sahara Desert with a group of awesome Berebers."
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
-              text="Travel trough the Islands of Bali in a Private Cruise"
-              label="Luxury"
-              path="/services"
-            />
-            <CardItem
-              src="images/img-2.jpg"
-              text="Travel trough the Islands of Bali in a Private Cruise"
-              label="Luxury"
+              src="images/img-4.jpg"
+              text="Play football in the most isolated football field in the world."
+              label="Weird"
               path="/services"
             />
           </ul>
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+
+function SignUp() {
+  return <h1 className="signup">SIGNUP</h1>;
+}
+
+export default SignUp;
