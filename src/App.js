c0ed8003c6ebbcc5b86eb2492c3a0b7d72bd9cf2
Jorge <jorgediazok@gmail.com>
2020-09-16 01:09:34 -0300

    fixed some bugs in the navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -12,6 +15,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/signup" exact component={SignUp} />
         </Switch>
       </Router>
     </React.Fragment>
